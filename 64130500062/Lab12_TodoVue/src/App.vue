744028b88721033a9d54ce17e2e8c09ab8995c79
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-11-15 18:33:56 +0700

    Complete Todos!

--- 64130500062/Lab12_TodoVue/src/App.vue
+++ 64130500062/Lab12_TodoVue/src/App.vue
@@ -1,35 +1,44 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { query, collection, getDocs } from 'firebase/firestore'
-  import db from './firebase/init.js'
-  import TodoList from './components/TodoList.vue'
-  import TodoInput from './components/TodoInput.vue'
+import { ref, onMounted } from 'vue'
+import { query, collection, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore'
+import db from './firebase/init.js'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
 
-  const todos = ref([])
+const todos = ref([])
 
-  async function getTodos(){
-    const querySnap = await getDocs(query(collection(db,'todos')))
-    todos.value = []
-    querySnap.forEach( (doc) => {
-      let data = doc.data() 
-      data.id = doc.id 
-      todos.value.push(data)
-    })
-  }
-
-  async function toggleCompleted(index){
-    /* add code for toggling the completed field */
-    console.log('toggle')
-  }
-
-  async function deleteTodo(index){
-    /* add code for deleting a todo */
-    console.log('delete')
-  }
-
-  onMounted(() => {
-    getTodos() 
+async function getTodos() {
+  const querySnap = await getDocs(query(collection(db, 'todos')))
+  todos.value = []
+  querySnap.forEach((doc) => {
+    let data = doc.data()
+    data.id = doc.id
+    todos.value.push(data)
   })
+}
+
+async function toggleCompleted(index) {
+  /* add code for toggling the completed field */
+  // console.log('toggle')
+  const todo = todos.value[index]
+  const todoRef = doc(db, 'todos', todos.value[index].id);
+  await updateDoc(todoRef, {
+    completed: !todo.completed
+  })
+  await getTodos()
+}
+
+async function deleteTodo(index) {
+  /* add code for deleting a todo */
+  // console.log('delete')
+  const todoRef = doc(db, 'todos', todos.value[index].id);
+  await deleteDoc(todoRef);
+  await getTodos();
+}
+
+onMounted(() => {
+  getTodos()
+})
 </script>
 
 <template>
@@ -38,6 +47,4 @@
   <TodoList :todos="todos" @toggle-completed="toggleCompleted" @delete-todo="deleteTodo" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- 64130500062/Lab12_TodoVue/src/components/TodoInput.vue
+++ 64130500062/Lab12_TodoVue/src/components/TodoInput.vue
@@ -1,20 +1,30 @@
 <script setup>
-    import { ref } from 'vue'
-    const emit = defineEmits(['update-todos'])
-    const newTitle = ref('')
+import db from "../firebase/init";
+import { addDoc, collection } from 'firebase/firestore';
+import { ref } from 'vue'
+const emit = defineEmits(['update-todos'])
+const newTitle = ref('')
 
-  async function addTodo(){
-    /* add code for create a new doc and send $emit('update-todos') */
-    console.log('add todo')
-  }
+async function addTodo() {
+  /* add code for create a new doc and send $emit('update-todos') */
+  // console.log('add todo')
+  if (!newTitle.value) return;
+  const newTodo = {
+    title: newTitle.value,
+    completed: false,
+    createdAt: new Date()
+  };
+  await addDoc(collection(db, 'todos'), newTodo);
+  newTitle.value = '';
+  emit('update-todos');
+}
 </script>
 
 <template>
-    <div>
-      <input v-model="newTitle" placeholder="Enter a new task" size="30" />
-      <button @click="addTodo">ADD</button>
-    </div>
+  <div>
+    <input v-model="newTitle" placeholder="Enter a new task" size="30" />
+    <button @click="addTodo">ADD</button>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
