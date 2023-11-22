c87460c324547ad13fb2064997eb85d9abbfe85c
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-11-22 22:33:46 +0700

    Init Lab13 & 14

---
+++ Lab13 & 14/UTF-8 Lab14_Query/Lab14_Query/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div id="header">
+    <h2>Reports</h2>
+    <RouterLink to="/query/1">1. Cities with population &gt; 100000</RouterLink>
+    <RouterLink to="/query/2">2. Cities in New York</RouterLink>
+  </div>
+  <div id="detail">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+a {
+  display: block;
+}
+</style>
