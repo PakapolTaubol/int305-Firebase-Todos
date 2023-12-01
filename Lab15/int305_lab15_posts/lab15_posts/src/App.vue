b469473b84df28f24bf83266347555ca7d745d6b
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-12-01 19:41:13 +0700

    init Lab15

---
+++ Lab15/int305_lab15_posts/lab15_posts/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import { query, collection, getDocs } from 'firebase/firestore'
+import db from './firebase/init.js'
+import UserList from './components/UserList.vue'
+
+const users = ref([])
+
+async function getUsers(){
+    /*  add your code here */
+}
+
+onMounted(() => {
+    getUsers() 
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      <UserList :users="users" />
+    </div>
+  </div>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
