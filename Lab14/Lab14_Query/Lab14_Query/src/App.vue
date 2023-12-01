c2905d43b8cc586185647f9d2a40477082969608
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-12-01 19:36:42 +0700

    Leb 14

--- Lab14/Lab14_Query/Lab14_Query/src/App.vue
+++ Lab14/Lab14_Query/Lab14_Query/src/App.vue
@@ -7,11 +7,14 @@ import { RouterLink, RouterView } from "vue-router";
     <h2>Reports</h2>
     <RouterLink to="/query/1">1. Cities with population &gt; 100000</RouterLink>
     <RouterLink to="/query/2">2. Cities in New York</RouterLink>
-    <RouterLink to="/query/3">3. List zips in DC with population > 40000</RouterLink>
-    <RouterLink to="/query/4">4. List zips in NY or DC with population > 60000</RouterLink>
-    <RouterLink to="/query/5">5. List zips in DC or cities with population > 100000</RouterLink>
+    <RouterLink to="/query/3">3. List zips in DC with population &gt; 40000</RouterLink>
+    <RouterLink to="/query/4">4. List zips in NY or DC with population &gt; 60000</RouterLink>
+    <RouterLink to="/query/5">5. List zips in DC or cities with population &gt; 100000</RouterLink>
     <RouterLink to="/query/6">6. List zips in DC and sort the data by population</RouterLink>
     <RouterLink to="/query/7">7. List zips latitude between 71 and 73</RouterLink>
+    <RouterLink to="/query/8">8. Count zips in DC or cities with population > 100000</RouterLink>
+    <RouterLink to="/query/9">9. show the total population in dc (state)</RouterLink>
+    <RouterLink to="/query/10">10. Show the average population in NY(state)</RouterLink>
   </div>
   <div id="detail">
     <RouterView />
--- Lab14/Lab14_Query/Lab14_Query/src/views/QueryView.vue
+++ Lab14/Lab14_Query/Lab14_Query/src/views/QueryView.vue
@@ -1,68 +1,97 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
-import { collection, query, where, getDocs, limit } from "firebase/firestore";
-import db from "../firebase/init.js";
-import Documents from "../components/Documents.vue";
-import { useRoute } from "vue-router";
+import { ref, onMounted, watch } from 'vue'
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  limit,
+  orderBy,
+  or,
+  sum,
+  getAggregateFromServer,
+  average,
+} from 'firebase/firestore'
+import db from '../firebase/init.js'
+import Documents from '../components/Documents.vue'
+import { useRoute } from 'vue-router'
 
-const zips = ref([]);
-const title = ref("");
-const route = useRoute();
+const zips = ref([])
+const title = ref('')
+const route = useRoute()
 
 async function getQuery() {
-  let qryId = route.params.id;
-  zips.value = [];
-  let qry = null;
+  let qryId = route.params.id
+  zips.value = []
+  let qry = null
   if (qryId == 1) {
-    title.value = "Cities with population > 100000";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, where("pop", ">", 100000));
+    title.value = 'Cities with population > 100000'
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('pop', '>', 100000))
   } else if (qryId == 2) {
-    title.value = "Cities in New York";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, where("state", "==", "NY"), limit(10));
+    title.value = 'Cities in New York'
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('state', '==', 'NY'), limit(10))
   } else if (qryId == 3) {
-    title.value = "List zips in DC with population > 40000";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, where("state", "==", "DC"), where("pop", ">", 40000));
+    title.value = 'List zips in DC with population > 40000'
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('state', '==', 'DC'), where('pop', '>', 40000))
   } else if (qryId == 4) {
-    title.value = "List zips in NY or DC with population > 60000";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, where("state", "in", ["NY", "DC"]), where("pop", ">", 60000));
+    title.value = 'List zips in NY or DC with population > 60000'
+    const zipsRef = collection(db, 'zips')
+    qry = query(
+      zipsRef,
+      where('state', 'in', ['NY', 'DC']),
+      where('pop', '>', 60000)
+    )
   } else if (qryId == 5) {
-    title.value = "List zips in DC or cities with population > 100000";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, or(where("state", "==", "DC"), where("pop", ">", 100000)));
+    title.value = 'List zips in DC or cities with population > 100000'
+    const zipsRef = collection(db, 'zips')
+    qry = query(
+      zipsRef,
+      or(where('state', '==', 'DC'), where('pop', '>', 100000))
+    )
   } else if (qryId == 6) {
-    title.value = "List zips in DC and sort the data by population";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef, where("state", "==", "DC"), orderBy("pop"));
+    title.value = 'List zips in DC and sort the data by population'
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('state', '==', 'DC'), orderBy('pop'))
   } else if (qryId == 7) {
-    title.value = "List zips latitude between 71 and 73";
-    const zipsRef = collection(db, "zips");
-    qry = query(zipsRef,
-      where("loc.x", ">=", 71),
-      where("loc.x", "<=", 73)
-    );
+    title.value = 'List zips latitude between 71 and 73'
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('loc.x', '>', 71), where('loc.x', '<', 73))
+  } else if (qryId == 8) {
+    title.value = 'Count zips in DC or cities with population > 100000'
+    const zipsRef = collection(db, 'zips')
+    qry = query(
+      zipsRef,
+      or(where('state', '==', 'DC'), where('pop', '>', '100000'))
+    )
+  } else if (qryId == 9) {
+    const zipsRef = collection(db, 'zips')
+    qry = await query(zipsRef, where('state', '==', 'DC'))
+    const snapshot = await getAggregateFromServer(qry, { totlapop: sum('pop') })
+    title.value = 'show the total population in dc ' + snapshot.data().totlapop
+  } else if (qryId == 10) {
+    const zipsRef = collection(db, 'zips')
+    qry = query(zipsRef, where('state', '==', 'NY'))
+    const snapshot = await getAggregateFromServer(qry, { totlapop: average('pop') })
+    title.value = 'Show the average population in NY(state) ' + snapshot.data().totlapop
   }
 
-  const querySnap = await getDocs(qry);
+  const querySnap = await getDocs(qry)
   querySnap.forEach((doc) => {
-    let data = doc.data();
-    data.id = doc.id;
-    zips.value.push(data);
-  });
+    let data = doc.data()
+    data.id = doc.id
+    zips.value.push(data)
+  })
 }
 
-watch(() => route.params.id, getQuery);
+watch(() => route.params.id, getQuery)
 
 onMounted(() => {
-  getQuery();
-});
-
+  getQuery()
+})
 </script>
 <template>
   <Documents :title="title" :data="zips" />
 </template>
-
-<style></style>
